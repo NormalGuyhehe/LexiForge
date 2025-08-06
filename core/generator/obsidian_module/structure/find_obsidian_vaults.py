6077c6008dfe258e65f3b00ef3218208d42390e4
admin <tokarevnikolai2006@gmail.com>
2025-08-06 16:31:01 +0500

    develop structure builder

--- core/generator/obsidian_module/structure/find_obsidian_vaults.py
+++ core/generator/obsidian_module/structure/find_obsidian_vaults.py
@@ -8,7 +8,7 @@ def find_obsidian_vaults():
     for mount in get_mount_points():
         root = Path(mount)
         try:
-            for path in root.rglob('.obsidian'):
+            for path in root.rglob('Главная'):
                 if path.is_dir():
                     vaults.append(path.parent)
                     print(f"[+] Найден Vault: {path.parent}")
--- core/generator/obsidian_module/structure_builder.py
+++ core/generator/obsidian_module/structure_builder.py
@@ -1,4 +1,5 @@
-from pathlib import *
+import os
+from pathlib import Path
 from functools import lru_cache
 from core.generator.obsidian_module.structure.find_obsidian_vaults import find_obsidian_vaults
 from core.generator.obsidian_module.structure.language_structure import *
@@ -8,9 +9,12 @@ async def structure_creator():
     for vault in vault_list:
         if (
             vault.is_dir and 
-            vault.name == "Главная" and
-            vault.parent.name == ".obsidian"
+            vault.name == "obsidian" and
+            vault.parent.name == "Documents"
             ):
-            for folder in directories:
-                path = vault / folder
-                # path.   
+                path_to_main = vault / "Главная"
+                os.chdir(str(path_to_main))
+                os.chdir("english")
+                current_dir = os.getcwd()
+                print(current_dir)
+                
